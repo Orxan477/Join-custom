2278c9a662cd3b976bf4188b61db14723ca86781
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-11-02 12:00:29 +0400

    Join

---
+++ Join-custom/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace Join_custom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region string type
+            //Console.WriteLine("Enter the size of the array:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //string[] str = new string[n];
+            //Console.WriteLine("Enter Array:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    str[i] = Convert.ToString(Console.ReadLine());
+            //}
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(Join(str));
+            #endregion
+
+            #region int type
+            //Console.WriteLine("Enter the size of the array:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] num = new int[n];
+            //Console.WriteLine("Enter Array:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(Join(num));
+            #endregion
+        }
+        /// <summary>
+        /// We add the string we want to the beginning or end of the words.
+        /// </summary>
+        /// <param name="str">Value of string type.</param>
+        /// <returns>Modified word</returns>
+         
+        //static string Join(string[] str)
+        //{
+        //    StringBuilder result = new StringBuilder();
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        result.Append("-" + str[i]);
+        //    }
+        //    return result.ToString();
+
+        //}
+
+        /// <summary>
+        /// We add the string we want to the beginning or end of the words.
+        /// </summary>
+        /// <param name="str">Value of int type.</param>
+        /// <returns>Modified number</returns>
+        
+        //static string Join(int[] str)
+        //{
+        //    StringBuilder result = new StringBuilder();
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        result.Append("-" + str[i]);
+        //    }
+        //    return result.ToString();
+
+        //}
+    }
+}
